d0dc60b95f469f5e881dace3984f157eb266cd66
Poul Henriksen <pohe@easj.dk>
2020-11-26 13:13:31 +0100

    Added Person and PersonCatalog

---
+++ GitTest2019/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest2019
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+    }
+}
